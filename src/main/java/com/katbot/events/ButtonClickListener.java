eaaceee9171f72845d6e8481a6fe3dad99516e07
dlt-v <tmm.michalski@gmail.com>
2023-12-17 22:17:00 +0100

    create basic button click listener

---
+++ src/main/java/com/katbot/events/ButtonClickListener.java
@@ -0,0 +1,23 @@
+package com.katbot.events;
+
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ButtonClickListener extends ListenerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(GuildMessageListener.class);
+    private static final String testingChannelID = System.getenv("testing-channel-id");
+    private static final String testingUserID = System.getenv("testing-user-id");
+
+    @Override
+    public void onButtonInteraction(@NotNull ButtonInteractionEvent event)
+    {
+        logger.info("Button was reacted with.");
+        event.getChannel().sendMessage("Button was interacted with.").queue();
+    }
+}
+
