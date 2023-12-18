63a358a693ca96fb92ce48ef21bfdaad4739a6bd
dlt-v <tmm.michalski@gmail.com>
2023-12-18 22:45:46 +0100

    add basic counting to polls

--- src/main/java/com/katbot/events/ButtonInteractionListener.java
+++ src/main/java/com/katbot/events/ButtonInteractionListener.java
@@ -1,10 +1,15 @@
 package com.katbot.events;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ButtonInteractionListener extends ListenerAdapter {
 
@@ -15,8 +20,28 @@ public class ButtonInteractionListener extends ListenerAdapter {
     @Override
     public void onButtonInteraction(@NotNull ButtonInteractionEvent event)
     {
-        logger.info("Button was reacted with.");
-        event.getChannel().sendMessage("Button was interacted with.").queue();
+
+        logger.info("Button \"" + event.getButton().getLabel() + "\" (id:" + event.getButton().getId() + ") was reacted with.");
+        event.reply("You chose: \"" + event.getButton().getLabel() + "\"").setEphemeral(true).queue();
+
+
+        MessageEmbed oldEmbed = event.getMessage().getEmbeds().get(0);
+
+        EmbedBuilder newEmbedBuilder = new EmbedBuilder();
+        newEmbedBuilder.setTitle(oldEmbed.getTitle());
+        newEmbedBuilder.setDescription(oldEmbed.getDescription());
+        newEmbedBuilder.setColor(oldEmbed.getColor());
+
+        List<MessageEmbed.Field> fields = oldEmbed.getFields();
+        for (MessageEmbed.Field field : fields) {
+            int value = Integer.parseInt(field.getValue());
+            if (field.getName().equals(event.getButton().getLabel())) value++;
+            newEmbedBuilder.addField(field.getName(), String.valueOf(value), false);
+        }
+        List<MessageEmbed> embeds = new ArrayList<>();
+        embeds.add(newEmbedBuilder.build());
+        event.getMessage().editMessageEmbeds(embeds).queue();
+
     }
 }
 
