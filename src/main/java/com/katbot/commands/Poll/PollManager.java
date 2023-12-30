c13c26472828ae97da80db96586e58446ac72517
dlt-v <tmm.michalski@gmail.com>
2023-12-30 15:34:30 +0100

    convert polls from hashset to hashmap for quicker lookup times

--- src/main/java/com/katbot/commands/Poll/PollManager.java
+++ src/main/java/com/katbot/commands/Poll/PollManager.java
@@ -1,28 +1,44 @@
 package com.katbot.commands.Poll;
 
-import java.util.HashSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
 
 public class PollManager {
-    private HashSet<Poll> polls;
+    private HashMap<Long, Poll> polls;
+    private static PollManager instance;
+    private static final Logger logger = LoggerFactory.getLogger(PollManager.class);
 
-    public PollManager() {
-        polls = new HashSet<>();
+    private PollManager() {
+        polls = new HashMap<>();
+    }
+    public static PollManager getInstance() {
+        if (instance == null) {
+            instance = new PollManager();
+        }
+        return instance;
     }
 
     public void addPoll(Poll poll) {
-        polls.add(poll);
+        polls.put(poll.getId(), poll);
     }
 
-    public boolean addVote(int pollId, Vote vote) {
-        for (Poll poll : polls) {
-            if (poll.getId() == pollId) {
-                if (poll.getVotes().contains(vote)) {
-                    return false;
-                }
-                poll.addVote(vote);
-                return true;
-            }
+    public Poll getPoll(long id) {
+        return polls.get(id);
+    }
+
+    public boolean addVote(long pollId, Vote vote) {
+        Poll poll = getPoll(pollId);
+        if (poll == null) {
+            logger.error("Vote was not added. This poll is not cached anymore in memory.");
+            return false;
         }
-        return false;
+        if (poll.containsVote(vote.userId())) {
+            logger.error("Vote was not added. User with id: " + vote.userId() + " already voted in this poll.");
+            return false;
+        }
+        poll.addVote(vote);
+        return true;
     }
 }
