1e9bb3f647aaf25531238125edb70fe831c52c49
Tomasz Michalski <tmm.michalski@gmail.com>
2024-09-01 15:58:46 +0200

    feat: remove the init.sql from docker compose

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users
-(
-    id        SERIAL PRIMARY KEY,
-    username  VARCHAR(50) NOT NULL,
-    joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-INSERT INTO users (username) VALUES ('alice');
